ac18de0bb861d579aa06cd3cf7d6cf781aa24aea
Daniel Quispe Herrera <william.quispe2497@icloud.com>
2023-07-08 17:19:11 -0500

    Agregar vista PokemonDetail y subcomponentes

---
+++ app/src/main/java/com/ac/pokemonapp/PokemonDetailView.kt
@@ -0,0 +1,124 @@
+package com.ac.pokemonapp
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.zIndex
+import androidx.constraintlayout.compose.ConstraintLayout
+
+@Composable
+fun PokemonDetailView() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color(0xFFE94219))
+    ) {
+        ConstraintLayout(modifier = Modifier.fillMaxSize()) {
+
+            val (descriptionSection, backgroundSection, mainImage) = createRefs()
+            val topGuideline = createGuidelineFromTop(0.4f)
+
+            Image(
+                painter = painterResource(id = R.drawable.ic_background_detail),
+                contentDescription = null,
+                modifier = Modifier
+                    .zIndex(1f)
+                    .padding(horizontal = 4.dp)
+                    .constrainAs(backgroundSection) {
+                        top.linkTo(topGuideline)
+                        linkTo(start = parent.start, end = parent.end)
+                        bottom.linkTo(parent.bottom)
+                    }
+            )
+
+            Image(
+                painter = painterResource(id = R.drawable.pikachu),
+                contentDescription = null,
+                contentScale = ContentScale.Fit,
+                modifier = Modifier
+                    .zIndex(2f)
+                    .size(200.dp)
+                    .constrainAs(mainImage) {
+                        linkTo(start = parent.start, end = parent.end)
+                        top.linkTo(backgroundSection.top)
+                        bottom.linkTo(backgroundSection.top)
+                    }
+            )
+
+            Column(
+                modifier = Modifier
+                    .zIndex(2f)
+                    .constrainAs(descriptionSection) {
+                        linkTo(start = parent.start, end = parent.end)
+                        bottom.linkTo(parent.bottom)
+                    }
+            ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = "About",
+                        fontSize = 24.sp,
+                        fontFamily = FontFamily.Monospace,
+                        fontWeight = FontWeight.ExtraBold,
+                        color = Color.White,
+                        modifier = Modifier.weight(1.5f)
+                    )
+                    Text(
+                        text = "All",
+                        fontSize = 14.sp,
+                        textAlign = TextAlign.End,
+                        color = Color.White,
+                        fontFamily = FontFamily.Monospace,
+                        modifier = Modifier.weight(0.5f)
+                    )
+                }
+
+
+                Row(
+                    modifier = Modifier.padding(horizontal = 12.dp)
+                ) {
+                    PokemonStats(modifier = Modifier.weight(1f), image = R.drawable.energy, title = "Attack", value = "124")
+                    PokemonStats(modifier = Modifier.weight(1f), image = R.drawable.protection,title = "Protection", value = "108")
+                    PokemonStats(modifier = Modifier.weight(1f), image = R.drawable.healt,title = "Health", value = "70")
+                    PokemonStats(modifier = Modifier.weight(1f), image = R.drawable.pokeball,title = "Chance to Catch", value = "%16")
+                }
+
+                PokemonDescription(modifier = Modifier.padding(16.dp))
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PokemonDetailViewPreview() {
+    MaterialTheme {
+        PokemonDetailView()
+    }
+}
---
+++ app/src/main/java/com/ac/pokemonapp/PokemonInfo.kt
@@ -0,0 +1,171 @@
+package com.ac.pokemonapp
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.VerticalAlignmentLine
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun PokemonStats(modifier: Modifier = Modifier, image: Int? = null, title: String, value: String) {
+    Box(
+        modifier = modifier
+            .padding(horizontal = 4.dp)
+            .height(140.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(color = Color.White, shape = RoundedCornerShape(12.dp))
+                .padding(horizontal = 12.dp, vertical = 8.dp),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(id = image ?: R.drawable.ic_launcher_background),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(60.dp)
+                    .padding(top = 8.dp, bottom = 8.dp)
+            )
+            Text(
+                text = title,
+                fontSize = 10.sp,
+                lineHeight = 16.sp,
+                textAlign = TextAlign.Center,
+                fontFamily = FontFamily.Monospace,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 4.dp),
+            )
+            Text(
+                text = value,
+                fontSize = 18.sp,
+                fontWeight = FontWeight.ExtraBold,
+                fontFamily = FontFamily.Monospace
+            )
+        }
+    }
+}
+
+
+@Composable
+fun PokemonDescription(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier
+            .background(color = Color.White, shape = RoundedCornerShape(12.dp))
+            .fillMaxWidth()
+            .height(200.dp)
+            .padding(horizontal = 12.dp, vertical = 16.dp)
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Where to catch Pikachu",
+                fontSize = 18.sp,
+                fontWeight = FontWeight.ExtraBold,
+                fontFamily = FontFamily.Monospace
+            )
+            Text(
+                text = "You can get Pikachu Pokémon from 2 km eggs, and" +
+                        "there is also a chance to catch it. You can catch" +
+                        "Pikachu at the very beginning of the game, when" +
+                        "the irst 3 Pokémon aobear on the mao.",
+                fontSize = 10.sp,
+                textAlign = TextAlign.Center,
+                lineHeight = 16.sp,
+                fontFamily = FontFamily.Monospace,
+                modifier = Modifier.padding(vertical = 8.dp)
+            )
+            Spacer(modifier = Modifier.weight(1f))
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Button(
+                    onClick = { },
+                    colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFF9E2E0)),
+                    border = BorderStroke(width = 8.dp, color = Color(0xFFF9E2E0))
+                ) {
+                    Text(
+                        text = "To Favorites",
+                        fontFamily = FontFamily.Monospace,
+                        fontWeight = FontWeight.ExtraBold,
+                        color = Color(0xFFE94219)
+                    )
+                }
+                Spacer(modifier = Modifier.width(12.dp))
+                Button(
+                    onClick = { },
+                    colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFE94219)),
+                    border = BorderStroke(width = 8.dp, color = Color(0xFFE94219))
+                ) {
+                    Text(
+                        text = "Open the Map",
+                        fontFamily = FontFamily.Monospace,
+                        fontWeight = FontWeight.ExtraBold,
+                        color = Color(0xFFF9E2E0)
+                    )
+                }
+            }
+
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PokemonStatsPreview() {
+    MaterialTheme {
+        Row {
+            PokemonStats(modifier = Modifier.weight(1f), title = "Attack", value = "124")
+            PokemonStats(modifier = Modifier.weight(1f), title = "Protection", value = "108")
+            PokemonStats(modifier = Modifier.weight(1f), title = "Health", value = "70")
+            PokemonStats(modifier = Modifier.weight(1f), title = "Chance to Catch", value = "%16")
+        }
+
+    }
+}
+
+@Preview
+@Composable
+fun PokemonDescriptionPreview() {
+    MaterialTheme {
+        PokemonDescription()
+    }
+}
